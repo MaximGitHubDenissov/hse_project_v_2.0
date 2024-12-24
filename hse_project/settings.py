41a64dc3cc8694d757134b3eb9db37f09b29fc82
Maxim Denissov <DM7894289@gmail.com>
2024-12-24 14:03:05 +0500

    Mistake in settings.py

--- hse_project/settings.py
+++ hse_project/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
         'HOST': 'denissovtestserver.mysql.pythonanywhere-services.com',
         'OPTIONS':{
-            'init_command': "SET_NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
         },
     }
