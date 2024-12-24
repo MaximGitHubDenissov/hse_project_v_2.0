f602de6f11e226469f75c56593077360606c4bc1
Maxim Denissov <DM7894289@gmail.com>
2024-12-24 14:04:47 +0500

    Mistake in settings.py

--- hse_project/settings.py
+++ hse_project/settings.py
@@ -154,7 +154,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR/'static'
 
 
